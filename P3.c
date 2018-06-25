55ecdae8a12e248498d14970e68ab7b9486a4800
gsmelo <38022852+gsmelo@users.noreply.github.com>
2018-06-24 23:30:43 -0300

    Update P3.c

--- P3.c
+++ P3.c
@@ -3,277 +3,283 @@
 
 #include <stdio.h>
 
-int main ()
-{	int arg1, arg2, i=0, opc=0, resultado;
-	int vet1[256], vet2[256], vet3[256];
-	unsigned int contador = 2147483648;;
-do {	printf ("\n Bem vindo (a)! Escolha uma das opcoes a seguir: ");
-	printf ("\n 1 - NOT");//1
-	printf ("\n 2 - AND"); //2
-	printf ("\n 3 - OR");//2
-	printf ("\n 4 - XOR"); //2
-	printf ("\n 5 - Right Shift");//1
-	printf ("\n 6 - Left Shift"); //1
-	printf ("\n\n Digite sua opcao: ");	
-	scanf ("%d", &opc);
-	switch(opc)
-	        {   case 1:
-	        	printf("\nDigite o argumento: ");
-	        	scanf("%i", &arg1);
-	        	getchar();
-	        	printf("NOT %i ( ", arg1);
-	        	for(contador; contador>0; contador>>= 1)
-					{	if (contador & arg1){
-							vet1[i]=1;
-						}else {
-							vet1[i]=0;
-						}
-						printf("%i", vet1[i]);
-						i++;
-					}
-				printf(" ) \n = ");
-				contador = 2147483648;
-				i=0;
-				printf("%d ( ", ~arg1);
-				for(contador; contador>0; contador>>= 1)
-					{printf("%i", not vet1[i]);
-						i++;}		printf(" ) ");
-				
-				break;
-			case 2:	
-	               	printf("\nDigite o primeiro argumento: ");
-	        	scanf("%i", &arg1);
-	        	getchar();
-	        	printf("\nDigite o segundo argumento: ");
-	        	scanf("%i", &arg2);
-	        	getchar();
-	        	printf("\n\n %d ( ", arg1);
-	        	contador = 2147483648;
-				for(contador; contador>0; contador>>= 1)
-					{
-						if (contador & arg1){
-							vet1[i]=1;
-						} else {
-							vet1[i]=0;
-						}
-						printf("%i", vet1[i]);
-						i++;
-					}	printf(" ) ");
-				printf("\n AND ");
-				printf("\n %d ( ", arg2);
-				contador = 2147483648;
-				i=0;
-				for(contador; contador>0; contador>>= 1)
-					{
-						if (contador & arg2){
-							vet2[i]=1;
-						}
-						else {
-							vet2[i]=0;
-						}
-						printf("%i", vet2[i]);
-						
-						i++;
-						}					
-				printf(" ) : ");
-				contador = 2147483648;
-				i=0;
-				printf("%d ( ", (arg1&arg2));
-				for(contador; contador>0; contador>>= 1)
-				{
-				vet3[i] = vet2[i] & vet1[i];
-				printf("%i", vet3[i]);
-				i++;
-				}printf(" ) ");
-				
-				break;			
-				case 3: 
-			printf("\nDigite o primeiro argumento: ");
-	        	scanf("%i", &arg1);
-	        	getchar();
-	        	printf("\nDigite o segundo argumento: ");
-	        	scanf("%i", &arg2);
-	        	getchar();
-	        	printf("\n\n %d ( ", arg1);
-	        	contador = 2147483648;
-				for(contador; contador>0; contador>>= 1)
-					{
-						if (contador & arg1){
-							vet1[i]=1;
-						}
-						else {
-							vet1[i]=0;
-						}
-						printf("%i", vet1[i]);
-						
-						i++;
-					}	printf(" ) ");
-				printf("\n OR ");
-				printf("\n %d ( ", arg2);
-				contador = 2147483648;
-				i=0;
-				for(contador; contador>0; contador>>= 1)
-					{
-						if (contador & arg2){
-							vet2[i]=1;
-						}else {
-							vet2[i]=0;
-						}
-						printf("%i", vet2[i]);
-						i++;
-					}			
-				printf(" ) : ");
-				contador = 2147483648;
-				i=0;
-				printf ("%d ( ", arg1 | arg2);
-				for(contador; contador>0; contador>>= 1)
-				{
-				vet3[i] = vet2[i] or vet1[i];
-				printf("%i", vet3[i]);
-				i++;
-				}	printf(" ) ");
-			
-				break;
-				case 4: 
-			printf("\nDigite o primeiro argumento: ");
-	        	scanf("%i", &arg1);
-	        	getchar();
-	        	printf("\nDigite o segundo argumento: ");
-	        	scanf("%i", &arg2);
-	        	getchar();
-	        	printf("\n\n %d ( ", arg1);
-	        	contador = 2147483648;
-				for(contador; contador>0; contador>>= 1)
-					{
-						if (contador & arg1){
-							vet1[i]=1;
-						}else {
-							vet1[i]=0;
-						}
-						printf("%i", vet1[i]);
-						i++;
-					}	printf(" ) ");
-				printf("\n XOR ");
-				printf("\n %d ( ", arg2);
-				contador = 2147483648;
-				i=0;
-				for(contador; contador>0; contador>>= 1)
-					{
-						if (contador & arg2){
-							vet2[i]=1;
-						}else {
-							vet2[i]=0;
-						}
-						printf("%i", vet2[i]);
-						i++;
-					}
-				printf(" ) : ");
-				contador = 2147483648;
-				i=0;
-				printf("%d ( ", arg1 ^ arg2);
-				for(contador; contador>0; contador>>= 1)
-				{
-				vet3[i] = vet2[i] xor vet1[i];
-				printf("%i", vet3[i]);
-				i++;
-				}printf(" ) ");
-				
-				break;
-				case 5:
-			printf("\nDigite o primeiro argumento: ");
-	        	scanf("%i", &arg1);
-	        	getchar();
-	        	printf("\nDigite o segundo argumento: ");
-	        	scanf("%i", &arg2);
-	        	getchar();
-	        	printf("\n\n %d ( ", arg1);
-	        	resultado = arg1 >> arg2;
-	        	contador = 2147483648;
-	        	i=0;
-	        	for(contador; contador>0; contador>>= 1)
-					{if (contador & arg1){
-							vet1[i]=1;
-						}else {
-							vet1[i]=0;
-						}
-						printf("%i", vet1[i]);
-						i++;
-					}printf(" ) ");
-					printf("\n >> ");
-				printf("\n %d ( ", arg2);
-				contador = 2147483648;
-	        	i=0;
-		 for(contador; contador>0; contador>>= 1)
-					{if (contador & arg2){
-							vet2[i]=1;
-						}else {
-							vet2[i]=0;
-						}
-						printf("%i", vet2[i]);
-						i++;
-					}
-				contador = 2147483648;
-				i=0;
-				printf(" ) : ");
-				printf("%d ( ", arg1 >> arg2);
-				for(contador; contador>0; contador>>= 1)
-					{if (contador & resultado)
-						{vet3[i]=1;
-						}
-						else {
-							vet3[i]=0;
-						}
-						printf("%i", vet3[i]);
-						i++;
-					}	printf(" ) ");
-		 		break;
-				case 6: 
-					printf("\nDigite o primeiro argumento: ");
-	        	scanf("%i", &arg1);
-	        	getchar();
-	        	printf("\nDigite o segundo argumento: ");
-	        	scanf("%i", &arg2);
-	        	getchar();
-	        	printf("\n\n %d ( ", arg1);
-	        	resultado = arg1 << arg2;
-	        	contador = 2147483648;
-	        	i=0;
-	        	for(contador; contador>0; contador>>= 1)
-					{if (contador & arg1){
-							vet1[i]=1;
-						}	else {
-							vet1[i]=0;
-						}
-						printf("%i", vet1[i]);
-						i++;
-					}printf(" ) ");
-					printf("\n << ");
-				printf("\n %d ( ", arg2);
-				contador = 2147483648;
-	        	i=0;
-	        	for(contador; contador>0; contador>>= 1)
-					{if (contador & arg2){
-							vet2[i]=1;
-						}else {
-							vet2[i]=0;
-						}
-						printf("%i", vet2[i]);
-						i++;
-					}
-				contador = 2147483648;
-				i=0;
-				printf(" ) : ");
-				printf("%d ( ", arg1 << arg2);
-				for(contador; contador>0; contador>>= 1)
-					{if (contador & resultado)
-						{
-							vet3[i]=1;
-						}else {
-							vet3[i]=0;
-						}
-						printf("%i", vet3[i]);
-						i++;
-					}printf(" ) ");}} 
-while(opc==0);
-return 0;
+int main()
+{	int opcao, a, b, c = 0, bina[32],binb[32], binc[32], i=0,j;;
+	do{
+	printf("1 - NOT\n2 - AND\n3 - OR\n4 - XOR\n5 - Right Shift\n6 - Left Shift");
+	printf("\n\nDIGITE: ");
+	scanf("%d", &opcao);
+	getchar();
+	switch(opcao)
+	{
+	case 1:
+	printf("Digite um numero inteiro A: ");
+	scanf("%d", &a);
+	getchar();
+	c = ~a;
+	printf("NOT %d (", a);
+	if(a < 0)
+	a = a*(-1);
+	while(a>0){
+	bina[i] = a % 2;
+	i++;
+	a = a / 2;
+	}
+	for(j = i; j<32; j++)
+	bina[j] = 0;
+	for(j = 31; j>=0; j--){
+	if(bina[j] == 1)
+	printf("0");
+	else
+	printf("1");
+	}
+	printf(")");
+	i=0;
+	j=0;
+	printf(": %d (", c);
+	while(c>0){
+	binc[i] = c % 2;
+	i++;
+	c = c / 2;
+	}
+	for(j = i; j<32; j++)
+	binc[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binc[j]);
+	printf(")");	
+	return 0;
+	case 2:
+	printf("Digite um numero inteiro A: ");
+	scanf("%d", &a);
+	getchar();
+	printf("Digite um numero inteiro B: ");
+	scanf("%d", &b);
+	c = a & b;
+	printf("%d (", a);
+	while(a>0){
+	bina[i] = a % 2;
+	i++;
+	a = a / 2;
+	}
+	for(j = i; j<32; j++)
+	bina[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", bina[j]);
+	printf(")");	
+	i=0;
+	j=0;	
+	printf("AND %d (", b);
+	while(b>0){
+	binb[i] = b % 2;
+	i++;
+	b = b / 2;
+	}
+	for(j = i; j<32; j++)
+	binb[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binb[j]);
+	printf(")");
+	i=0;
+	j=0;
+	printf(": %d (", c);
+	while(c>0){
+	binc[i] = c % 2;
+	i++;
+	c = c / 2;
+	}
+	for(j = i; j<32; j++)
+	binc[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binc[j]);
+	printf(")");
+	return 0;
+	case 3:
+	printf("Digite um numero inteiro A: ");
+	scanf("%d", &a);
+	getchar();
+	printf("Digite um numero inteiro B: ");
+	scanf("%d", &b);
+	c = a | b;
+	printf("%d (", a);
+	while(a>0){
+	bina[i] = a % 2;
+	i++;
+	a = a / 2;
+	}
+	for(j = i; j<32; j++)
+	bina[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", bina[j]);
+	printf(")");	
+	i=0;
+	j=0;	
+	printf("OR %d (", b);
+	while(b>0){
+	binb[i] = b % 2;
+	i++;
+	b = b / 2;
+	}
+	for(j = i; j<32; j++)
+	binb[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binb[j]);
+	printf(")");
+	i=0;
+	j=0;
+	printf(": %d (", c);
+	while(c>0){
+	binc[i] = c % 2;
+	i++;
+	c = c / 2;
+	}
+	for(j = i; j<32; j++)
+	binc[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binc[j]);
+	printf(")");
+	return 0;
+	case 4:
+	printf("Digite um numero inteiro A: ");
+	scanf("%d", &a);
+	getchar();
+	printf("Digite um numero inteiro B: ");
+	scanf("%d", &b);
+	c = a ^ b;
+	printf("%d (", a);
+	while(a>0){
+	bina[i] = a % 2;
+	i++;
+	a = a / 2;
+	}
+	for(j = i; j<32; j++)
+	bina[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", bina[j]);
+	printf(")");	
+	i=0;
+	j=0;	
+	printf("XOR %d (", b);
+	while(b>0){
+	binb[i] = b % 2;
+	i++;
+	b = b / 2;
+	}
+	for(j = i; j<32; j++)
+	binb[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binb[j]);
+	printf(")");
+	i=0;
+	j=0;
+	printf(": %d (", c);
+	while(c>0){
+	binc[i] = c % 2;
+	i++;
+	c = c / 2;
+	}
+	for(j = i; j<32; j++)
+	binc[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binc[j]);
+	printf(")");
+	return 0;
+	case 5:
+	printf("Digite um numero inteiro A: ");
+	scanf("%d", &a);
+	getchar();
+	printf("Digite um numero inteiro B: ");
+	scanf("%d", &b);
+	c = a >> b;
+	printf("%d (", a);
+	while(a>0){
+	bina[i] = a % 2;
+	i++;
+	a = a / 2;
+	}
+	for(j = i; j<32; j++)
+	bina[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", bina[j]);
+	printf(")");	
+	i=0;
+	j=0;	
+	printf("Right Shift %d (", b);
+	while(b>0){
+	binb[i] = b % 2;
+	i++;
+	b = b / 2;
+	}
+	for(j = i; j<32; j++)
+	binb[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binb[j]);
+	printf(")");
+	i=0;
+	j=0;
+	printf(": %d (", c);
+	while(c>0){
+	binc[i] = c % 2;
+	i++;
+	c = c / 2;
+	}
+	for(j = i; j<32; j++)
+	binc[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binc[j]);
+	printf(")");
+	return 0;
+	case 6:
+	printf("Digite um numero inteiro A: ");
+	scanf("%d", &a);
+	getchar();
+	printf("Digite um numero inteiro B: ");
+	scanf("%d", &b);
+	c = a << b;
+	printf("%d (", a);
+	if(a < 0)
+	a = a*(-1);
+	while(a>0){
+	bina[i] = a % 2;
+	i++;
+	a = a / 2;
+	}
+	for(j = i; j<32; j++)
+	bina[j] = 0;
+	for(j = 31; j>=0; j--){
+	if(bina[j] == 1)
+	printf("0");
+	else
+	printf("1");
+	}
+	printf(")");
+	i=0;
+	j=0;	
+	printf("Left Shift %d (", b);
+	while(b>0){
+	binb[i] = b % 2;
+	i++;
+	b = b / 2;
+	}
+	for(j = i; j<32; j++)
+	binb[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binb[j]);
+	printf(")");
+	i=0;
+	j=0;
+	printf(": %d (", c);
+	while(c>0){
+	binc[i] = c % 2;
+	i++;
+	c = c / 2;
+	}
+	for(j = i; j<32; j++)
+	binc[j] = 0;
+	for(j = 31; j>=0; j--)
+	printf("%d", binc[j]);
+	printf(")");
+	return 0;
+	}
+	}while(opcao);
+	return 0;
 }
