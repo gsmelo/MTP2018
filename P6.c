aa6dc90f2d12b2d7427e0ed1c807bfa57dcb7332
gsmelo <38022852+gsmelo@users.noreply.github.com>
2018-07-08 17:04:02 -0300

    Update P6.c

--- P6.c
+++ P6.c
@@ -3,59 +3,33 @@
 
 #include <stdio.h>
 
-int soma(int vet[20], int n);
-float media(int soma, float cont);
-void preenchezero(int vet[20]);
-
-int main()
-	{
-		int vet[20];
-		int i, somatorio, tam;
-		float cont;
-		printf("Digite a quantidade de termos desejados: ");
-		scanf("%d", &tam);
-		preenchezero(vet);
-		for(i=0; i<tam; i++)
-		{
-			printf("\nDigite o %d numero: ", i+1);
-			scanf("%d", &vet[i]);
-			getchar();
-			if(vet[i] != 0) 
-			cont++;
-		}
-		somatorio = (soma (vet, i) );
-		printf("\nA media dos numeros digitados eh: %f", media(somatorio, cont));
-		
-		return 0;
-		}
-		
-	
-void preenchezero(int vet[20])
-	{
-		int i;
-		for(i=0; i<20; i++)
-		{
-			vet[i] = 0;
-		}
-	}
-	
-int soma(int vet[20], int n) 
-	{
-		int s;
-	    if (n == 0) return 0;
-	    else{
-		      s = soma(vet, n-1);
-		      if (vet[n-1] > 0)
-			  	{
-				  s += vet[n-1];
-				}
-		      return (s);
-			}
-	}
-	
 float media(int soma, float cont)
-	{
-		float resul;
-		resul = soma/cont;
-		return resul;
-	}
+{
+	float resul;
+	resul = soma/cont;
+	return resul;
+}
+
+int soma_vet(int v[20], int n) 
+{
+      if(n == 0) return 0;
+      else return(v[n-1] + soma_vet(v,n-1));
+}
+      
+int main()
+{
+	int i, num[20], soma, tam;
+	float cont = 0.0f;
+	for(i=0; i<20; i++){
+		num[i] = 0;}
+	printf("\nDigite o numero de termos: ");
+	scanf("%d", &tam);
+	for(i=0; i<tam; i++){
+		printf("\nDigite o %d numero: ", i+1);
+		scanf("%d", &num[i]);
+		getchar();
+		if(num[i] != 0) cont++;}
+	soma = (soma_vet(num, tam));
+	printf("\n%f", media(soma, cont));
+	return 0;
+}
